615af5052d4fc4ace291528380c777a3ca85be4f
juaelopezlop <juaelopezlop@unal.edu.co>
2022-12-03 22:08:52 -0500

    p1

--- preguntas.py
+++ preguntas.py
@@ -13,19 +13,20 @@ def pregunta_01():
     Complete el código presentado a continuación.
     """
     # Importe pandas
-    import ___ as ___
+    import pandas as pd
 
     # Importe PolynomialFeatures
-    from ___ import ___
+    from sklearn import preprocessing
 
     # Cargue el dataset `data.csv`
-    data = ___.___("___")
+    # data = pd.read_csv("C:/Users/Dellphoto/Documents/Universidad/Analitica predictiva/data.csv")
+    data = pd.read_csv("data.csv")
 
     # Cree un objeto de tipo `PolynomialFeatures` con grado `2`
-    poly = ___.___(___)
+    poly = preprocessing.PolynomialFeatures(2)
 
     # Transforme la columna `x` del dataset `data` usando el objeto `poly`
-    x_poly = poly.___(data[["___"]])
+    x_poly = poly.fit_transform(data[["x"]])
 
     # Retorne x y y
     return x_poly, data.y
